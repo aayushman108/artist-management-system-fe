f80ce70b17ac438516a79c82fa7eea91d10aae9a
Aayushman <aayushmansharma108@gmail.com>
2026-06-01 20:56:36 +0545

    chore(types): rename types dir to @types

--- src/@types/api.d.ts
+++ src/@types/api.d.ts
@@ -1,9 +1,7 @@
 declare namespace Api {
   interface BaseResponse<T> {
     success: boolean;
-    data: {
-      message: string;
-      data: T;
-    };
+    message: string;
+    data: T;
   }
 }
