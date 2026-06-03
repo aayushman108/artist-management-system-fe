ecae24d7e79e912bf135ea940e87463725353938
Aayushman <aayushmansharma108@gmail.com>
2026-06-04 00:08:33 +0545

    chore(types): rename auth.d.ts file to user.d.ts, and add user related types

---
+++ src/@types/user.d.ts
@@ -0,0 +1,45 @@
+import type { UserRoleType } from "../constants/general.constant";
+
+declare namespace User {
+  interface IUser {
+    id: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    role: UserRoleType;
+    created_by: string | null;
+    status: string;
+    created_at: string;
+    updated_at: string;
+  }
+
+  interface ILoginResponse {
+    user: IUser;
+    accessToken: string;
+  }
+
+  interface IUserProfile {
+    dob: string | null;
+    gender: string | null;
+    address: string | null;
+    phone: string | null;
+    avatar: {
+      public_id: string;
+      url: string;
+    } | null;
+  }
+
+  interface IArtist {
+    stage_name: string | null;
+    manager_id: string | null;
+    artist_manager_name: string | null;
+  }
+
+  interface IExtendedUser {
+    user: IUser & { creator_name: string; creator_role: UserRoleType };
+    profile: IUserProfile | null;
+    artist: IArtist | null;
+  }
+
+  type IPaginatedUserResponse = Api.PaginatedData<IExtendedUser>;
+}
