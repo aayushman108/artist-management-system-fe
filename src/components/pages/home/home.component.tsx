4283593d3c0198dd71ccce19aeca5b1ae73f11ef
Aayushman <aayushmansharma108@gmail.com>
2026-06-10 05:39:55 +0545

    feat(pages): add home page and not-found page

---
+++ src/components/pages/home/home.component.tsx
@@ -0,0 +1,9 @@
+import styles from "./home.module.scss";
+
+export function HomePage() {
+  return (
+    <div className={styles.homePage}>
+      <h1>Welcome to Artist Management System</h1>
+    </div>
+  );
+}
